02651d66e1b75019c0c01dcc12bfaa2e2c77c67f
Hirotachi <hirotachi@epical.me>
2018-09-19 02:00:04 +0100

    add game functionality styles and complete message

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,10 @@
 const strings = ["lucky", "the lake", "tester testing"];
 const keyboard = "abcdefghijklmnopqrstuvwxyz";
-const liveHeart = `<li><img src="images/liveHeart.png" alt="alive heart"><li>`
-let current = "test";
+const liveHeart = `<li><img src="images/liveHeart.png" alt="alive heart"></li>`;
+const deadHeart = "images/lostHeart.png";
+const liveSrc = "images/liveHeart.png";
+const $hearts = $(".hearts ul");
+
 $(".game").hide();
 
 
@@ -15,34 +18,79 @@ $(".start-btn").on("click", function(e){
    
 // insert random string from strings array into an li in .word_fill ul
 
-   const randIndex = Math.floor(Math.random() * strings.length); //generate random index based on strings length
-   for(let i = 0; i<strings[randIndex].length; i++){
-    $(".word_fill").append(`<li class="placeholder">${strings[randIndex][i]}</li>`);
+ 
+    
+});
+
+const randIndex = Math.floor(Math.random() * strings.length); //generate random index based on strings length
+const current = strings[randIndex];
+
+for(let i = 0; i<strings[randIndex].length; i++){
+ $(".word_fill").append(`<li class="placeholder">${strings[randIndex][i]}</li>`);
+  //get current string in screen and assign it to "current"
+ }
+
+ // removes spaces in string and replaces it by adding margin to the prev letter
+ $(".placeholder").each(function(){
+      
+     if($(this).text() === " "){
+         $(this).prev().css("marginRight", "3rem");
+         $(this).hide();
+     }
+     $(this).text("");
+ });
+
+ // add letter from the keyboard array
+ for(let i = 0; i< 26; i++){
+     const index = Math.floor(Math.random() * keyboard.length);
+     $(".keyboard").append(`<li class="keyboard_letter">${keyboard[i]}</li>`);
+ }
+
+//  place current word letters randomly in keyboard
+ 
+
+
+// add live hearts to screen
+for(i = 0; i < 5; i++){
+$hearts.append(liveHeart)
+}
+
+// place selected letter into its place if its right letter if not change next heart to deadHeart
+let place = 0;
+$(".keyboard_letter").on("click", function()  {
+    if(current.indexOf($(this).text()) >= 0){
+        for (let i = 0; i < current.length; i++){
+            if(current[i] === $(this).text()){
+                $(".placeholder").eq(i).text($(this).text()).addClass("letterFound");
+                $(this).addClass("found").off("click");
+            }
+        }
+    }else {
+        $hearts.children().eq(place).children()[0].src = deadHeart;
+        place++;
+        $(this).addClass("notFound").off("click");
     }
 
-    // removes spaces in string and replaces it by adding margin to the prev letter
-
-    $(".placeholder").each(function(){
-         current = $(this).text(); //get current string in screen and assign it to "current"
-        if($(this).text() === " "){
-            $(this).prev().css("marginRight", "3rem");
-            $(this).remove();
+// hide the game if the place is bigger than 5 and show the welcome message for now and rest the hreats src
+    if(place > 4){
+        $(".game").hide();
+        $(".greeting").show();
+        place = 0;
+        for(let i = 0; i < 5; i++){
+            $hearts.children().eq(i).children()[0].src = liveSrc;
         }
-        $(this).text(" ");
-        console.log(current);
-    });
+        $(".placeholder").each(function(){
+            $(this).text("");
+        });
+    }
 
-    // 
-    for(let i = 0; i< 20; i++){
-        const index = Math.floor(Math.random() * keyboard.length);
-        $(".keyboard").append(`<li class="keyboard_letter">${keyboard[index]}</li>`);
+    // check if the word complete is the same as the one from strings and show end game
+    if($(".word_fill").text().trim() === current.replace(" ", "")){
+        $(".word_fill").html(`<li class="complete">${current}<li>`);
     }
     
 });
 
 
 
-for(let i = 0; i < 5; i++){
 
-    $(".hearts").append(liveHeart);
-}
